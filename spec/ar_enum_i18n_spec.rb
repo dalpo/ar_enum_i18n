36602f6c7fdc8e945067d291523361cb6e5a6826
Andrea Dal Ponte <info@andreadalponte.com>
2018-06-14 11:47:27 +0200

    Fix: fake test

--- spec/ar_enum_i18n_spec.rb
+++ spec/ar_enum_i18n_spec.rb
@@ -2,10 +2,6 @@ require "spec_helper"
 
 RSpec.describe ArEnumI18n do
   it "has a version number" do
-    expect(Ar::Enum::I18n::VERSION).not_to be nil
-  end
-
-  it "does something useful" do
-    expect(false).to eq(true)
+    expect(ArEnumI18n::VERSION).not_to be nil
   end
 end
